b8d9a0f6660a72c057710c038e12f33544542d6a
dhsj8405 <dhsj8405@naver.com>
2021-11-24 12:24:44 +0900

    jsx tutorials #2

---
+++ 01.basics/ex07/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';      //jsx여서 리액트를 부르진않지만  컴포넌트만들때 createelement로 바뀌므로 import하고있어야함
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Hello React!!!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ 01.basics/ex08/src/01/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function() {
+    return (
+        <div>
+            <h2>App01</h2>
+            <p>JSX Tutorials - 특징1: HTML과 차이점</p>
+            {/*
+                1. 속성은 Camel Case
+            */}
+            <input type='text' maxLength='10' />
+            {/*
+                2. Element는 꼭 닫혀야 한다.
+                오류) <br>, <hr>, <input type='text'>, <img src=''>
+            */}
+            <br/>
+            <br/>
+            <img src='https://i.pinimg.com/474x/cf/78/d0/cf78d004d72aae79823e40d87ded742b.jpg' />
+            {/*
+                3. 속성 이름은 DOM API 기반이다.
+                    <div id='box' class ='box'>....</div>
+                    document.getElementById('box').className = 'box'
+            */}
+            <div id='box' className ='box'>
+                box 입니다.
+            </div>
+        </div>
+    )
+}
