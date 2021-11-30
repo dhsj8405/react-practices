3ab276042dd1cfb197ea917fcd05c89c850a5189
dhsj8405 <dhsj8405@naver.com>
2021-11-30 11:23:12 +0900

    event #1

---
+++ 02.component/ex03/src/02/TitleBar02.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+export default function TitelBar02() {
+    useState()
+    const onClickHandler = () =>{
+        // no VS state.no
+        console.log('TitleBar01 clicked');
+    }
+
+    return (
+        <h1
+         
+        onClick={onClickHandler}
+        style ={{cursor: 'pointer'}}>
+        ex03 - Functional Event Handler(Functional Component)</h1>
+    )
+}
