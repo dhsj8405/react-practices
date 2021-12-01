175481c2d1580818d9e590e55e095ce742977632
dhsj8405 <dhsj8405@naver.com>
2021-12-01 11:23:30 +0900

    제어 컴포넌트

--- 02.component/ex04/src/01/Incrementor02.js
+++ 02.component/ex04/src/01/Incrementor02.js
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
 
 export default function ({ begin, step }) {
-    const [stateVal, setVal] = useState(begin);
-    const [stateStep] = useState(step);
+    const [stateVal, setVal] = useState(begin);         // begin : 초기값, useState의 리턴값 =  배열을(setVal:우리가지정하는변수)하나 리턴해줌 ,stateVal: 분해된 배열
+    //const [stateStep] = useState(step);                 // 참조,안쓰겠다. readonly상태 , 필요없음
 
     return (
         <div>
@@ -13,6 +13,10 @@ export default function ({ begin, step }) {
             </button>
             {' '}
             <span>{ stateVal }</span>
+            {' '}
+            <button>
+                <strong>+</strong>
+            </button>
         </div>
     );
 }
