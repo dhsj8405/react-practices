0e0d84321334f848f568bed44100ad90d527b215
dhsj8405 <dhsj8405@naver.com>
2021-12-02 15:40:35 +0900

    kanbanboard #4

---
+++ kanbanboard/src/KanbanBoard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CardList from './CardList';
+
+import styles from './assets/css/KanbanBoard.css';
+import cards from './assets/json/data.json';
+
+export default function() {
+    return (
+        <div className={styles.KanbanBoard}>
+            <CardList key='ToDo' title={'ToDo'} cards={cards.filter((card) => card.status === 'ToDo') }/>
+            <CardList key='Doing' title={'Doing'} cards={cards.filter((card) => card.status === 'Doing') }/>
+            <CardList key='Done' title={'Done'} cards={cards.filter((card) => card.status === 'Done') }/>        
+        </div>
+    )
+}
