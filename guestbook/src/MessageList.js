c0f39cd28ac7819373d496ef0ad4c516c74d9e35
dhsj8405 <dhsj8405@naver.com>
2021-12-03 15:47:02 +0900

    component life

--- guestbook/src/MessageList.js
+++ guestbook/src/MessageList.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useRef } from 'react';
+import React, {Fragment, useState, useRef, useEffect} from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-modal';
 import Message from './Message';
@@ -7,46 +7,55 @@ import modalStyles from "./assets/scss/modal.scss";
 
 Modal.setAppElement('body');
 
-export default function MessageList({ messages }) {
+export default function MessageList({messages}) {
     const refForm = useRef(null);
-    const [modalData, setModalData] = useState({ isOpen: false });
+    const [modalData, setModalData] = useState({isOpen: false});
 
-    const handleSubmit = async(e) => {
+    useEffect(()=> {
+        setTimeout(() =>{
+            refForm.current && refForm.current.password.focus();
+        }, 200);
+    },[modalData]);
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            if (e.target.password.value === '') {
+            if(e.target.password.value === '') {
                 return;
             }
-            // const response = await fetch(`/api/${modalData.messageNo}`,{
+
+            // const response = await fetch(`/api/${modalData.messageNo}`, {
             //     method: 'delete',
             //     header: {
-            //         'Content-Type' : 'application/json',
-            //         'Accept' : 'application/json'
+            //         'Content-Type': 'application/json',
+            //         'Accept': 'application/json'
             //     },
             //     body: JSON.stringify({password: modalData.password})
-            // })
-            // if(!response.ok){
-            //     throw `${response.status} ${response.statusText}`
+            // });
+
+            // if(!response.ok) {
+            //     throw  `${response.status} ${response.statusText}`;
             // }
 
             // const jsonResult = response.json;
 
+
             // 비밀번호가 틀린 경우
             // jsonResult.data = null
-            setModalData({}, Object.assign(modalData), {title: '....', password: ''});
+            setModalData({}, Object.assign(modalData), {title: '.....', password: ''});
             // 잘 삭제가 된 경우
             // jsonResult.data = 10
-            console.log("삭제!!!! : ", modalData);
+
+            console.log("삭제!!!:", modalData);
 
         } catch (err) {
             console.error(err);
         }
-
     }
 
     const notifyDeleteMessage = (no) => {
         setModalData({
-            title: '작성시 입력했던 비밀번호를 입력 하세요.',
+            title: '작성시 입력했던 비밀번호를 입력 하세요.', 
             isOpen: true,
             messageNo: no,
             password: ''
@@ -57,18 +66,18 @@ export default function MessageList({ messages }) {
         <Fragment>
             <ul className={styles.MessageList}>
                 {messages.map(message => <Message key={`guestbook_message_${message.no}`}
-                    no={message.no}
-                    name={message.name}
-                    message={message.message}
-                    notifyDeleteMessage={notifyDeleteMessage} />)}
+                                                  no={message.no}
+                                                  name={message.name}
+                                                  message={message.message} 
+                                                  notifyDeleteMessage={notifyDeleteMessage} />)}
             </ul>
             <Modal
                 isOpen={modalData.isOpen}
-                onRequestClose={() => setModalData({ isOpen: false })}
+                onRequestClose={ () => setModalData({isOpen: false}) }
                 shouldCloseOnOverlayClick={true}
                 className={modalStyles.Modal}
                 overlayClassName={modalStyles.Overlay}
-                style={{ content: { width: 350 } }}>
+                style={{content: {width: 350}}}>
                 <h1>비밀번호입력</h1>
                 <div>
                     <form
@@ -82,14 +91,14 @@ export default function MessageList({ messages }) {
                             name={'password'}
                             value={modalData.password}
                             placeholder={'비밀번호'}
-                            onChange={(e) => setModalData(Object.assign({}, modalData, { password: e.target.value }))} />
+                            onChange={(e) => setModalData(Object.assign({}, modalData, {password: e.target.value}))}/>
                     </form>
                 </div>
                 <div className={modalStyles['modal-dialog-buttons']}>
-                    <button onClick={() => {
-                        refForm.current.dispatchEvent(new Event("submit", { cancelable: true, bubbles: true }));
-                    }}>확인</button>
-                    <button onClick={() => { setModalData(Object.assign({}, modalData, { isOpen: false })) }}>취소</button>
+                    <button onClick={ () => {
+                        refForm.current.dispatchEvent(new Event("submit", {cancelable: true, bubbles: true}));
+                    } }>확인</button>
+                    <button onClick={() => {setModalData(Object.assign({}, modalData, {isOpen: false})) } }>취소</button>
                 </div>
             </Modal>
         </Fragment>
